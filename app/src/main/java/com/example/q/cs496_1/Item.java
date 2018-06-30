d824ea0e81ed3028b4b3c792f30cdbaa36facf2a
sheeha97 <sheeha97@users.noreply.github.com>
2018-06-30 15:40:33 +0900

    new stuff

---
+++ app/src/main/java/com/example/q/cs496_1/Item.java
@@ -0,0 +1,4 @@
+package com.example.q.cs496_1;
+
+public class Item {
+}
---
+++ app/src/main/java/com/example/q/cs496_1/MyAdapter.java
@@ -0,0 +1,6 @@
+package com.example.q.cs496_1;
+
+import android.support.v7.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter {
+}
