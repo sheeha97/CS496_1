256d08b8db29b58549c467cb4d9781f334839c26
sheeha97 <sheeha97@users.noreply.github.com>
2018-06-30 15:44:15 +0900

    added cnew fiels

--- app/src/main/java/com/example/q/cs496_1/Item.java
+++ app/src/main/java/com/example/q/cs496_1/Item.java
@@ -1,4 +1,25 @@
 package com.example.q.cs496_1;
 
 public class Item {
+    private String mName;
+    private String mPhone;
+    private String mHome;
+
+    public Item(String name, String phone, String home) {
+        mName = name;
+        mPhone = phone;
+        mHome = home
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone(){
+        return mPhone;
+    }
+
+    public String getHome() {
+        return mHome;
+    }
 }
--- app/src/main/java/com/example/q/cs496_1/MyAdapter.java
+++ app/src/main/java/com/example/q/cs496_1/MyAdapter.java
@@ -1,6 +1,21 @@
 package com.example.q.cs496_1;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
-public class MyAdapter extends RecyclerView.Adapter {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    public class  MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewName;
+        public TextView mTextViewPhone;
+        public TextView mTextViewHome;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            mTextViewName = itemView.findViewById(R.id.text_view_name);
+            mTextViewHome = itemView.findViewById(R.id.text_view_number);
+
+        }
+    }
 }
