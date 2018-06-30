5ae7e99ccd535883aa0e4b033a59f4c033b40ca1
sheeha97 <sheeha97@users.noreply.github.com>
2018-06-30 16:17:25 +0900

    hi

--- app/src/main/java/com/example/q/cs496_1/MyAdapter.java
+++ app/src/main/java/com/example/q/cs496_1/MyAdapter.java
@@ -1,10 +1,48 @@
 package com.example.q.cs496_1;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context mContext;
+    private ArrayList<Item> mList;
+
+    public MyAdapter(Context context, ArrayList<Item> myList) {
+        mContext = context;
+        mList = myList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.number_item, viewGroup, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        Item currentItem = mList.get(position);
+
+        String name = currentItem.getName();
+        String home = currentItem.getHome();
+        String phone = currentItem.getPhone();
+
+        myViewHolder.mTextViewName.setText("Name: " + name);
+        myViewHolder.mTextViewHome.setText("Home: " + home);
+        myViewHolder.mTextViewPhone.setText("Phone: " + phone);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
 
     public class  MyViewHolder extends RecyclerView.ViewHolder {
         public TextView mTextViewName;
